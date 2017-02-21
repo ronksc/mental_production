07ee07bbbef4f984ec4894e15b9556a8e3961660
Ron Chan <ksc.ron@gmail.com>
2017-02-21 19:40:57 +0800

    update Ron 20170221-19:40

--- wp-content/themes/mental_production/templates/content-home.php
+++ wp-content/themes/mental_production/templates/content-home.php
@@ -4,12 +4,15 @@
 			<div class="image_wrapper">
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_1.jpg" class="img-responsive" />
 			</div>
+            <div class="media-video"></div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item">
@@ -17,11 +20,13 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_2.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item double-height">
@@ -29,11 +34,13 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_3.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item">
@@ -41,11 +48,13 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_2.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item double-height">
@@ -53,11 +62,27 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_3.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
+			</div>		
+		</div>
+        <div class="grid_item width2x">
+			<div class="image_wrapper">
+				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_4.jpg" class="img-responsive" />
+			</div>
+			<div class="overlay_wrapper">
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item">
@@ -65,11 +90,13 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_1.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item">
@@ -77,11 +104,13 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_2.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item double-height">
@@ -89,11 +118,13 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_3.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item">
@@ -101,11 +132,13 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_2.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 		<div class="grid_item double-height">
@@ -113,11 +146,13 @@
 				<img src="<?=get_stylesheet_directory_uri()?>/assets/img/img_dummy_3.jpg" class="img-responsive" />
 			</div>
 			<div class="overlay_wrapper">
-				<div class="overlay">
-					<h3 class="overlay_title">Testing 123</h3>
-					<div class="overlay_category">Catgeory</div>
-				</div>
-				<a href="#" class="overlay_link"></a>
+            	<div class="overlay_content">
+                    <div class="overlay">
+                        <h3 class="overlay_title">Testing 123</h3>
+                        <div class="overlay_category">Catgeory</div>
+                    </div>
+                    <a href="#" class="overlay_link"></a>
+                </div>
 			</div>		
 		</div>
 	</div>
