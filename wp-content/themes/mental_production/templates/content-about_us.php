bc0a7f6e42eb2e607c501a3768a95afa4e381ad0
Ron Chan <ksc.ron@gmail.com>
2017-03-16 18:26:49 +0800

    update Ron 21070316-18:26

--- wp-content/themes/mental_production/templates/content-about_us.php
+++ wp-content/themes/mental_production/templates/content-about_us.php
@@ -3,10 +3,12 @@
 		$feature_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
 	}
 ?>
-
+<div class="img-container visible-xs visible-sm hidden-md hidden-lg">
+    <img src="<?=$feature_image_url[0]?>" class="img-responsive" />
+</div>
 <div class="container-fluid">
-	<div class="clearfix" style="background-image:url(<?=$feature_image_url[0]?>); background-size:cover; background-repeat:no-repeat;">
-        <div style="margin-left:40%; background:#fff; padding:40px 45px;">
+	<div class="clearfix container_bg" style="background-image:url(<?=$feature_image_url[0]?>); ">
+        <div class="about_content">
         <?php 
             $content = apply_filters('the_content', $post->post_content); 
 			echo $content;
