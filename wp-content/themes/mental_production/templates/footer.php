b7ee03e105e5fee5002c83eb1831fe989f7381a6
ronksc <ksc.ron@gmail.com>
2017-05-22 04:14:17 +0800

    update Ron 20170522-0414

--- wp-content/themes/mental_production/templates/footer.php
+++ wp-content/themes/mental_production/templates/footer.php
@@ -12,7 +12,7 @@
 		<?php
             //Main menu
             if (has_nav_menu('primary_navigation')) :
-              wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'depth' => 0));
+              wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav mobile', 'depth' => 0));
             endif;
         ?>
     </nav>
